74a29815c07969e01cde8ad8a5bc29d7f573b205
katiadaly <dalyk@etown.edu>
2026-08-27 10:26:20 -0400

    Katia student page

---
+++ web/studentpages/katia_daly.php
@@ -0,0 +1,32 @@
+<?php
+$pageName = "Katia Daly's Webpage";
+require_once __DIR__ . "/../includes/functions.php";
+$url = url();
+$url = str_replace("/studentpages"," ", $url);
+require_once __DIR__ . "/../includes/header.php";
+require_once __DIR__ . "/../includes/navbar.php";
+
+?>
+   
+    <div class="buttons">
+        <a href="<?PHP echo $url;?>/about.php"><button class="button button2">Go Back!</button></a> 
+    </div>
+    <BR><BR>    
+    <main>
+        <section id="Katia Daly's Bio">
+            <div class="w3-card w3-container">
+                <div class="w3-panel w3-blue">
+                    <h2>Katia Daly</h2>
+                </div>
+            
+                <div id="w3-article"> Katia Daly is a student in this class. She is majoring in Computer Science, with a concentration in Web and Application 
+                    Design and intends to graduate in 2028.
+                    <BR><BR>
+                </div>
+            </div>
+        </section>
+    </main>
+</body>
+<?php
+     require_once __DIR__ . "/../includes/footer.php";
+?>
