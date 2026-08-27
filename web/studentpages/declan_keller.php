6b9b44540aa1c439a677953c80413a334fca3006
Your Name <you@example.com>
2026-08-27 10:22:42 -0400

    Keller 1st commit

---
+++ web/studentpages/declan_keller.php
@@ -0,0 +1,32 @@
+<?php
+$pageName = "Declan Keller's Webpage";
+require_once __DIR__ . "/../includes/functions.php";
+$url = url();
+$url = str_replace("/studentpages"," ", $url);
+require_once __DIR__ . "/../includes/header.php";
+require_once __DIR__ . "/../includes/navbar.php";
+
+?>
+   
+    <div class="buttons">
+        <a href="<?PHP echo $url;?>/about.php"><button class="button button2">Go Back!</button></a> 
+    </div>
+    <BR><BR>    
+    <main>
+        <section id="Declan Keller's Bio">
+            <div class="w3-card w3-container">
+                <div class="w3-panel w3-blue">
+                    <h2>Declan Keller</h2>
+                </div>
+            
+                <div id="w3-article"> Declan Keller is a student in this class. He is a junior student athlete 
+                    majoring in Computer Science with a concentration in Cyber Security, intending to graduate in 2028.
+                    <BR><BR>
+                </div>
+            </div>
+        </section>
+    </main>
+</body>
+<?php
+     require_once __DIR__ . "/../includes/footer.php";
+?>
