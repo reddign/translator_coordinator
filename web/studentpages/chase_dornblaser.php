7a8814d80567b53cb651e88fdd490480544583ca
dornblaserc <dornblaserc@etown.edu>
2026-08-27 10:25:43 -0400

    Chase's student file

---
+++ web/studentpages/chase_dornblaser.php
@@ -0,0 +1,32 @@
+<?php
+$pageName = "Chase Dornblaser's Webpage";
+require_once __DIR__ . "/../includes/functions.php";
+$url = url();
+$url = str_replace("/studentpages"," ", $url);
+require_once __DIR__ . "/../includes/header.php";
+require_once __DIR__ . "/../includes/navbar.php";
+
+?>
+   
+    <div class="buttons">
+        <a href="<?PHP echo $url;?>/about.php"><button class="button button2">Go Back!</button></a> 
+    </div>
+    <BR><BR>    
+    <main>
+        <section id="Chase Dornblaser's Bio">
+            <div class="w3-card w3-container">
+                <div class="w3-panel w3-blue">
+                    <h2>Chase Dornblaser</h2>
+                </div>
+            
+                <div id="w3-article"> Chase Dornblaser is a current senior computer engineering major intending to
+                    graduate in May 2027. I am hoping to find a job in systems engineering or software engineering after graduation.
+                    <BR><BR>
+                </div>
+            </div>
+        </section>
+    </main>
+</body>
+<?php
+     require_once __DIR__ . "/../includes/footer.php";
+?>
