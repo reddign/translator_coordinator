8a14f0fefc28908967279a9aff6b51249f117ab7
Frank Conte <contef@etown.edu>
2026-08-27 10:21:57 -0400

    Frank 3rd commit

---
+++ web/studentpages/frank_conte.php
@@ -0,0 +1,31 @@
+<?php
+$pageName = "Frank Conte's Webpage";
+require_once __DIR__ . "/../includes/functions.php";
+$url = url();
+$url = str_replace("/studentpages"," ", $url);
+require_once __DIR__ . "/../includes/header.php";
+require_once __DIR__ . "/../includes/navbar.php";
+
+?>
+   
+    <div class="buttons">
+        <a href="<?PHP echo $url;?>/about.php"><button class="button button2">Go Back!</button></a> 
+    </div>
+    <BR><BR>    
+    <main>
+        <section id="Frank Conte's Bio">
+            <div class="w3-card w3-container">
+                <div class="w3-panel w3-blue">
+                    <h2>Frank Conte</h2>
+                </div>
+            
+                <div id="w3-article"> Frank Conte is a Computer Science Major with a Mathematics Minor and a Cybersecurity Concentration. He intends on graduating in 2028. He enjoys running and playing viola and coding.
+                    <BR><BR>
+                </div>
+            </div>
+        </section>
+    </main>
+</body>
+<?php
+     require_once __DIR__ . "/../includes/footer.php";
+?>
