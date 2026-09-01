ebdfde96d2eadc817d2a581b1f802bdccee86268
mensahc <mensahc@etown.edu>
2026-09-01 10:20:50 -0500

    Add link

--- web/about.php
+++ web/about.php
@@ -5,9 +5,9 @@ include "includes/navbar.php";
 ?>
 <h2>Translator Coordinator - Software Engineering Class Project 2026</h2>
        <div class="w3-section" id="team-names">
-            <span class="name"><b>Engineers:</b> Mo Abouelnagah, Anand Bum-Erdene, <a href="studentpages/frank_conte.php">Frank Conte</a>, <a href="studentpages/katia_daly.php">Katia Daly</a>, <a href="studentpages/sdomboski.php">Scott Domboski<a>, <a href="studentpages/chase_dornblaser.php">Chase Dornblaser</a>, 
-            <a href='studentpages/aaron_frist.php'>Aaron Frist</a>, <a href="studentpages/declan_keller.php">Declan Keller</a>, <a href="studentpages/olha_kuptsova.php">Olha Kuptsova</a>, Chris Mensah, <a href='studentpages/kaiden.php'>Kaiden Miller</a>, <a href='studentpages/connor_price.php'>Connor Price</a>, <a href='studentpages/noah_peiffer.php'>Noah Peiffer</a>, Kathy Saldana-Renteria, Brandon Short, 
-            <a href='studentpages/dominic_traina.php'>Dominic Traina</a>, <a href='studentpages/sam_vossen.php'>Sam Vossen</a>, <a href='studentpages/isaac_widders.php'>Isaac Widders</a>,
+            <span class="name"><b>Engineers:</b> Mo Abouelnagah, Anand Bum-Erdene, Frank Conte, <a href="studentpages/katia_daly.php">Katia Daly</a>, <a href="studentpages/sdomboski.php">Scott Domboski<a>, <a href="studentpages/chase_dornblaser.php">Chase Dornblaser</a>, 
+            <a href='studentpages/aaron_frist.php'>Aaron Frist</a>, <a href="studentpages/declan_keller.php">Declan Keller</a>, <a href="studentpages/olha_kuptsova.php">Olha Kuptsova</a>, <a href='studentpages/chris_mensah.php'>Chris Mensah</a>, <a href='studentpages/kaiden.php'>Kaiden Miller</a>, <a href='studentpages/connor_price.php'>Connor Price</a>, <a href='studentpages/noah_peiffer.php'>Noah Peiffer</a>, Kathy Saldana-Renteria, Brandon Short, 
+            Dominic Traina, <a href='studentpages/sam_vossen.php'>Sam Vossen</a>, <a href='studentpages/isaac_widders.php'>Isaac Widders</a>,
                 Teddy Bazyar (TA), and <a href='studentpages/Prof_reddig.php'>Prof Reddig</a>
             </span>
         </div>
