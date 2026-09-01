a64b614fa216cefc82cdb104b4567bf4770cb740
Olha Kuptsova <olha.v.kuptsova@gmail.com>
2026-09-01 10:41:57 -0400

    Added a clickable area around the face and linked the info page to the name in the list

--- web/about.php
+++ web/about.php
@@ -5,7 +5,7 @@ include "includes/navbar.php";
 ?>
 <h2>Translator Coordinator - Software Engineering Class Project 2026</h2>
        <div class="w3-section" id="team-names">
-            <span class="name"><b>Engineers:</b> Mo Abouelnagah, Anand Bum-Erdene, Frank Conte, Katia Daly, Scott Domboski, <a href="studentpages/chase_dornblaser.php">Chase Dornblaser</a>, <a href='studentpages/aaron_frist.php'>Aaron Frist</a>, Declan Keller, Olha Kuptsova, Chris Mensah, <a href='studentpages/kaiden.php'>Kaiden Miller</a>, Connor Price, Kathy Saldana-Renteria, Brandon Short, <a href='studentpages/dominic_traina.php'>Dominic Traina</a>, <a href='studentpages/sam_vossen.php'>Sam Vossen</a>, <a href='studentpages/isaac_widders.php'>Isaac Widders</a>,
+            <span class="name"><b>Engineers:</b> Mo Abouelnagah, Anand Bum-Erdene, Frank Conte, Katia Daly, <a href="studentpages/sdomboski.php">Scott Domboski</a>, <a href="studentpages/chase_dornblaser.php">Chase Dornblaser</a>, <a href='studentpages/aaron_frist.php'>Aaron Frist</a>, Declan Keller, <a href="studentpages/olha_kuptsova.php">Olha Kuptsova</a>, Chris Mensah, Kaiden Miller, Connor Price, Kathy Saldana-Renteria, Brandon Short, <a href='studentpages/dominic_traina.php'>Dominic Traina</a>, <a href='studentpages/sam_vossen.php'>Sam Vossen</a>, <a href='studentpages/isaac_widders.php'>Isaac Widders</a>,
                 Teddy Bazyar (TA), and <a href='studentpages/Prof_reddig.php'>Prof Reddig</a>
             </span>
         </div>
@@ -16,6 +16,8 @@ include "includes/navbar.php";
                 <area shape="circle" coords="480,150,20" href="studentpages/kaiden.php">
                 <area shape="circle" coords="203,150,22" href="studentpages/sam_vossen.php">
                 <area shape="circle" coords="905, 130, 26" href="studentpages/chase_dornblaser.php">
+                <area shape="circle" coords="668,150,28" href="studentpages/declan_keller.php">
+                <area shape="circle" coords="125,175,19" href="studentpages/olha_kuptsova.php">
             </map>    
         </div>
 
