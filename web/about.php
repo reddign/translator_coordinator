c8ec45c028f32351ddc11b509ddbf9bbf71f8471
Frank Conte <contef@etown.edu>
2026-09-03 09:37:48 -0400

    Added circle link

--- web/about.php
+++ web/about.php
@@ -25,6 +25,8 @@ include "includes/navbar.php";
                 <area shape="circle" coords="65,190,22" href="studentpages/katia_daly.php">
                 <area shape="circle" coords="520,160,24" href="studentpages/noah_peiffer.php">
                 <area shape="circle" coords="566,140,24" href="studentpages/dominic_traina.php">
+                <area shape="circle" coords="785,140,24" href="studentpages/frank_conte.php">
+
             </map>    
         </div>
 
