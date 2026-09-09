df04612bfc7141877da37c72f8a84042b3d9a50e
Nancy Reddig <reddign@etown.edu>
2026-09-09 14:55:12 -0400

    Add error reporting on index.php.

--- web/index.php
+++ web/index.php
@@ -1,4 +1,8 @@
 <?php
+
+ini_set('display_errors', 1);
+error_reporting(E_ALL & ~E_NOTICE);
+
 require_once dirname(__DIR__,1) . "/includes/config.php";
 require_once dirname(__DIR__,1) . "/includes/WFDatabase.php";
 
