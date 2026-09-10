c2b821664c8cd4591586a60f4b28cad46a2b336f
Your Name <you@example.com>
2026-09-10 08:59:11 -0400

    changes again

--- web/studentpages/sdomboski.php
+++ web/studentpages/sdomboski.php
@@ -23,7 +23,7 @@ require_once __DIR__ . "/../includes/navbar.php";
                                         He will be graduating in the spring of 2028.
                                         Scott is a silly boi who plays lax.
                     <BR><BR>
-                    Scott is the 2nd worse (sureeee ig) ITS employee yuh ( he's alright i guess) chillin
+                    Scott is the 2nd worse ITS employee(does he like cheese he's alright i guess) chillin
                 </div>
             </div>
         </section>
