c1990ab2046ffcd20fee5b9825fdcbbb40b97f38
Olha Kuptsova <olha.v.kuptsova@gmail.com>
2026-09-10 09:20:51 -0400

    Help, why do I not get the updates.. (added some comments to the translator.php btw)

--- web/translator.php
+++ web/translator.php
@@ -1,5 +1,6 @@
 <?php 
 
+// Just to push at least something because I do not understand why I do not get any updates that other people do.
 require_once __DIR__ . "/../includes/config.php";
 require_once __DIR__ . "/../includes/WFDatabase.php";
 
@@ -15,6 +16,7 @@ $usersResponse  = getJSONFromURL($usersDataURL);
 // Access your data
 $users = $usersResponse["data"];
 
+// Hopefully at some point this will get the users' data and put it on the page.
 $users = $users[0];
     echo "<h1>";
     echo $users["FIRST_NAME"];
