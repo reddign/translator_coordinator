1b1c0873d7569b55f222901f5aecf884f87906ce
IsaacWidders <isaacwidders@gmail.com>
2026-09-10 09:11:39 -0400

    Conflict

--- web/studentpages/isaac_widders.php
+++ web/studentpages/isaac_widders.php
@@ -19,7 +19,7 @@ require_once __DIR__ . "/../includes/navbar.php";
                     <h2>Isaac Widders</h2>
                 </div>
             
-                <div id="w3-article"> Isaac Widders is a student in this class. He is majoring in Computer Science, with a concentration in Software Development, and intends to graduate in 2029.
+                <div id="w3-article"> Isaac Widders is a student in this class. He is majoring in Computer Science, with a concentration in Software Development, and intends to graduate in 2029. Differnt
                     <BR><BR>
                 </div>
             </div>
