c98e005c08d028069dffe75e7ef9aaa5fc6ac3f9
Dominic Traina <trainad@etown.edu>
2026-09-10 09:16:00 -0400

    more changes from the conflictmaster

--- web/translatorEditor.php
+++ web/translatorEditor.php
@@ -11,6 +11,6 @@ Hehe take this Merge Master
 
 //stuff
 
-
+//I am editing this page for the purpose of causing a preventable error to occur. This is so that I can demonstrate conflict resolution and get points for this assignment. I am the resolution master.
 
 ?>
