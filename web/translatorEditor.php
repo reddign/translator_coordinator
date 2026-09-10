37d1461c421ff271100a12f8f65397636423609e
Dominic Traina <trainad@etown.edu>
2026-09-10 09:05:59 -0400

    commitmaster reporting for duty, creating conflict

---
+++ web/translatorEditor.php
@@ -0,0 +1,16 @@
+/**
+This file will hold the translator editor page
+This page will allow the user to edit their translation status and submit to update the database with their changes
+
+
+*/
+
+<?php
+//add includes
+//header?
+
+//stuff
+
+
+
+?>
