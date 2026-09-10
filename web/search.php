3aa133bda5915987a88fd6c6017331edc216e6c2
pricec1 <conpri806@gmail.com>
2026-09-10 09:52:11 -0400

    Added search bar template and basic js listener for automatic updates. Will update search results with specified criteria later.

--- web/search.php
+++ web/search.php
@@ -1,10 +1,41 @@
 <?php
+require_once __DIR__ . "/../includes/config.php";
+require_once __DIR__ . "/../includes/WFDatabase.php";
+
+if (isset($_GET['query'])) {
+    $search_string = trim($_GET['query']);
+    if (!empty($search_string)) {
+        // SQL Statement using search bar input
+        $sql = "SELECT * FROM wf_countries WHERE country_name LIKE '$search_string%'";
+
+        // Using WFDatabase functions to prevent redundancy
+        $results = WFDatabase::getDataFromSQL($sql);
+        // Give results from SQL search to javascript file
+        if ($results) {
+            /**echo "<pre>";
+            print_r($results);
+            echo "</pre>";*/
+            foreach ($results as $row) {
+                echo "<div class='search-item'>" . htmlspecialchars($row['COUNTRY_NAME']) . "</div>";
+            }
+        } else {
+            echo "<div class='no-results'>No results found.</div>";
+        }
+    }
+    exit;
+}
+
 include "includes/functions.php";
 include "includes/header.php";
 include "includes/navbar.php";
+
+
 ?>
 <h2>Translator Coordinator - Search Feature</h2>
-       <div class="w3-section" id="team-names">
+        <!-- Link javascript file to do searching -->
+        <!-- js script makes it so search automatically updates with each key input -->
+        <script src="search.js" defer></script>
+        <div class="w3-section" id="team-names">
             <span class="name">Once completed you will be able to search for translators by language, country or region.
                 You will also be able to search for groups to join that have interest in the lanugae you are learning.
             </span>
@@ -12,14 +43,15 @@ include "includes/navbar.php";
         <h3>Stay tuned for more updates.</h3>
         </div>
 
- 
-        
-        <BR><BR><BR><BR>
+        <BR>
+        <!-- Search bar -->
+        <div class="topnav">
+            <input type="text" id="search-bar" placeholder="Search..">
+        </div>
+
+        <div id="results"></div>
+
 
 <?php
-
-
-
 include "includes/footer.php";
-
 ?>
