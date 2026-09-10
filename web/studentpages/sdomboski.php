5eea2eaa87df614ae67fee78c290f1dfa22975dc
Sam Vossen <sam@jpsdomain.org>
2026-09-10 08:30:11 -0400

    messing with scott's page

--- web/studentpages/sdomboski.php
+++ web/studentpages/sdomboski.php
@@ -22,6 +22,7 @@ require_once __DIR__ . "/../includes/navbar.php";
                 <div id="w3-article"> Scott is an Information Systems major who is learing how to develop software in CS341!
                                         He will be graduating in the spring of 2028
                     <BR><BR>
+                    Scott is the 2nd worse ITS employee
                 </div>
             </div>
         </section>
