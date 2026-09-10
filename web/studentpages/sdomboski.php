53a614fe81402f163e0afd4bf6e6ab64ae732f27
Your Name <you@example.com>
2026-09-10 08:30:38 -0400

    Scoot changes by Declan

--- web/studentpages/sdomboski.php
+++ web/studentpages/sdomboski.php
@@ -20,7 +20,8 @@ require_once __DIR__ . "/../includes/navbar.php";
                 </div>
             
                 <div id="w3-article"> Scott is an Information Systems major who is learing how to develop software in CS341!
-                                        He will be graduating in the spring of 2028
+                                        He will be graduating in the spring of 2028.
+                                        Scott is a silly boi who plays lax.
                     <BR><BR>
                     Scott is the 2nd worse ITS employee
                 </div>
