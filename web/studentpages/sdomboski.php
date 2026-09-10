d63222b421731a87ad359078e76345f12717677e
ksaldrent2006 <k.saldana50123@gmail.com>
2026-09-10 08:58:58 -0400

    change

--- web/studentpages/sdomboski.php
+++ web/studentpages/sdomboski.php
@@ -23,7 +23,7 @@ require_once __DIR__ . "/../includes/navbar.php";
                                         He will be graduating in the spring of 2028.
                                         Scott is a silly boi who plays lax.
                     <BR><BR>
-                    Scott is the 2nd worse (sureeee ig) ITS employee( he's alright i guess) chillin
+                    Scott is the 2nd worse (sureeee ig) ITS employee yuh ( he's alright i guess) chillin
                 </div>
             </div>
         </section>
