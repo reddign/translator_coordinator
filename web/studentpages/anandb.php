364e8c097fcbf906f0163aa1e0cb1dc829f82874
Anand Bum-Erdene <anandbumaa@gmail.com>
2026-09-10 09:26:35 -0400

    conflict

--- web/studentpages/anandb.php
+++ web/studentpages/anandb.php
@@ -19,7 +19,7 @@ require_once __DIR__ . "/../includes/navbar.php";
                     <h2>Anand Bum-Erdene</h2>
                 </div>
             
-                <div id="w3-article"> Anand Bum-Erdene is a student in this class majoring in Computer Science with a concentration of Cyber Security. Intends to graduate spring 2027 after graduation wants to find a job in Cyber Scurity. Enjoys skiing and painting.
+                <div id="w3-article"> Anand Bum-Erdene is a student in this class majoring in Computer Science with a concentration of Cyber Security. Intends to graduate spring 2027 after graduation wants to find a job in Cyber Scurity.
                     <BR><BR>
                 </div>
             </div>
