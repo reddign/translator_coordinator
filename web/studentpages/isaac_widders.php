2b129bc1ac2a4a5b3faafaf6d5b6a8f3e6a0664b
Scott Domboski <sdomboski@gmail.com>
2026-09-10 08:44:33 -0400

    make a conflict

--- web/studentpages/isaac_widders.php
+++ web/studentpages/isaac_widders.php
@@ -20,6 +20,7 @@ require_once __DIR__ . "/../includes/navbar.php";
                 </div>
             
                 <div id="w3-article"> Isaac Widders is a student in this class. He is majoring in Computer Science, with a concentration in Software Development, and intends to graduate in 2029.
+                    I really hope this makes a conflict
                     <BR><BR>
                 </div>
             </div>
