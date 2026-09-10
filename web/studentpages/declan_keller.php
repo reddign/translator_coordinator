5af7ef0938137bd21a29fd4125025efc4b0adc49
ksaldrent2006 <k.saldana50123@gmail.com>
2026-09-10 08:23:57 -0400

    kathy's changes

--- web/studentpages/declan_keller.php
+++ web/studentpages/declan_keller.php
@@ -19,8 +19,9 @@ require_once __DIR__ . "/../includes/navbar.php";
                     <h2>Declan Keller</h2>
                 </div>
             
-                <div id="w3-article"> Declan Keller is a student in this class. He is a junior student athlete 
+                <div id="w3-article"> Declan Keller is a student in this class. He is a freshman student athlete 
                     majoring in Computer Science with a concentration in Cyber Security, intending to graduate in 2028.
+    
                     <BR><BR>
                 </div>
             </div>
