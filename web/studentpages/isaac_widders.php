6757a54c78e08adbf8a9d29ced2430026e102b76
IsaacWidders <isaacwidders@gmail.com>
2026-09-10 15:08:39 -0400

    Conflict

--- web/studentpages/isaac_widders.php
+++ web/studentpages/isaac_widders.php
@@ -19,10 +19,9 @@ require_once __DIR__ . "/../includes/navbar.php";
                     <h2>Isaac Widders</h2>
                 </div>
             
-                <div id="w3-article"> Isaac Widders is a student in this class. He is majoring in Computer Science, with a concentration in Software Development, and intends to graduate in 2029. make an issu
-                    So Isaac can make a conflict.
-                
-                    <BR><BR>
+                <div id="w3-article"> Isaac Widders is a student in this class. He is majoring in Computer Science, with a concentration in Software Development, and intends to graduate in 2029. make an issue        
+                    this is an edit
+                            <BR><BR>
                 </div>
             </div>
         </section>
