ec745a911f7714aa7bd639733fd7b2d9ab3043e0
Mo Abouelnagah <abouelnagahm@etown.edu>
2026-09-11 12:45:32 -0400

    added comment

--- web/studentpages/chris_mensah.php
+++ web/studentpages/chris_mensah.php
@@ -19,7 +19,7 @@ require_once __DIR__ . "/../includes/navbar.php";
                     <h2>Chris Mensah</h2>
                 </div>
             
-                <div id="w3-article">  Chris is majoring in Computer Engineering, and intends to graduate in 2027.
+                <div id="w3-article">  Chris is majoring in Computer Engineering, and intends to graduate in 2027. likes skiing.
                     <BR><BR>
                 </div>
             </div>
