3980bd9148453d714be2fdb3eaf2828737e016fb
mensahc <mensahc@etown.edu>
2026-09-17 10:50:12 -0500

    Stub function

--- web/includes/functions.php
+++ web/includes/functions.php
@@ -56,8 +56,8 @@ function searchTranslators($languageId = null, $languageName = null, $countryNam
         [
             'name'     => 'Placeholder Name',
             'id'        => $languageId,
-            'language' => '$languages',
-            'country'  => 'Placeholder Country'
+            'language' => $languages,
+            'country'  => $countryId
         ]
     ];
 }
