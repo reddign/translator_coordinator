471e86981efe61dd99acf4c1479af47be96b5c74
NP-Code01 <noahpeiffer555@gmail.com>
2026-09-17 12:15:07 -0400

    First Pull Request From Team Gamma (#2)
    * Stub Code for Team Gamma
    
    Co-authored-by: Scott Domboski <sdomboski@gmail.com>
    Co-authored-by: IsaacWidders <isaacwidders@gmail.com>

---
+++ web/feedGroupsGamma.php
@@ -0,0 +1,5 @@
+<? PHP
+
+# This file will be used for give the user a feed of differnt groups that the user can join.
+
+?>
---
+++ web/groupPopupGamma/joinGroupGamma.js
@@ -0,0 +1,1 @@
+//In this file we will allow a user to join a group in a popup window.
