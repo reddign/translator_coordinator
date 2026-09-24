eba2f731f8f4815599c7b765f70f062d45377fcc
mensahc <mensahc@etown.edu>
2026-09-24 08:31:53 -0500

    Stub function

--- web/search.php
+++ web/search.php
@@ -81,33 +81,33 @@ include "includes/navbar.php";
 </div>
 
 <?php
-// GET Filters
+//Checks if filter data exists before trying to get it. Also sets default to null incase of failure.
 $language = $_GET['language'] ?? null;
-$country  = $_GET['country']  ?? null;
-$region   = $_GET['region']   ?? null;
-$group    = $_GET['group']    ?? null;
-$id       = $_GET['id']       ?? null;
+$country  = $_GET['country'] ?? null;
+$region   = $_GET['region'] ?? null;
+$group    = $_GET['group'] ?? null; 
 
-$countries  = [];
-$currencies = [];
-$languages  = [];
+$id = $_GET['id'] ?? null;
 
-// Fetch API data only if ID exists
-if ($id) {
-    $countryResponse  = getJSONFromURL("{$mainURL}/api/countries/{$id}");
-    $currencyResponse = getJSONFromURL("{$mainURL}/api/currencies?countryid={$id}");
-    $languageResponse = getJSONFromURL("{$mainURL}/api/languages?countryid={$id}");
+$countryDataURL  = "{$mainURL}/api/countries/{$id}";
+$currencyDataURL = "{$mainURL}/api/currencies?countryid={$id}";
+$languageDataURL = "{$mainURL}/api/languages?countryid={$id}";
 
-    $countries  = $countryResponse['data']  ?? [];
-    $currencies = $currencyResponse['data'] ?? [];
-    $languages  = $languageResponse['data'] ?? [];
-}
+$countryResponse  = getJSONFromURL("{$mainURL}/api/countries/{$id}");
+$currencyResponse = getJSONFromURL("{$mainURL}/api/currencies?countryid={$id}");
+$languageResponse = getJSONFromURL("{$mainURL}/api/languages?countryid={$id}");
+
+$countries  = $countryResponse['data'] ?? [];
+$currencies = $currencyResponse['data'] ?? [];
+$languages  = $languageResponse['data'] ?? [];
+
 
 $countryName = $countries[0]['COUNTRY_NAME'] ?? '';
-$countryId   = $countries[0]['COUNTRY_ID']   ?? $id;
+$countryId   = $countries[0]['COUNTRY_ID'] ?? $id;
 
-// Parameters aligned: ($userName, $languageName, $countryName, $regionName, $group)
-$results = searchTranslators(null, $language, $countryName, $region, $group);
+
+// Call stub function. Currently a placeholder for demos.
+$results = searchTranslators($language, $country, $region, $group);
 ?>
 
 <h2>Search Results</h2>
@@ -117,11 +117,7 @@ $results = searchTranslators(null, $language, $countryName, $region, $group);
     
     <ul>
         <?php foreach ($results as $translator): ?>
-            <li>
-                <?= htmlspecialchars($translator['name'] ?? '') ?> - 
-                <?= htmlspecialchars($translator['language'] ?? '') ?> 
-                (<?= htmlspecialchars($translator['country'] ?? '') ?>)
-            </li>
+            <li><?= $translator['name'] ?> - <?= $translator['language'] ?> (<?= $translator['country'] ?>)</li>
         <?php endforeach; ?>
     </ul>
 </div>
