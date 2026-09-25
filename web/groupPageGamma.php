034ccac466362b061c0fc34122be04f764d03f9c
NP-Code01 <noahpeiffer555@gmail.com>
2026-09-24 22:43:23 -0400

    Add the Create Group button to the Group Page

--- web/groupPageGamma.php
+++ web/groupPageGamma.php
@@ -10,3 +10,8 @@ include "includes/functions.php";
 include "includes/header.php";
 include "includes/navbar.php";
 ?>
+
+<!--Create Group button that sends user to createGroupForm.php to create a group.-->
+<a href="/translator_coordinator/web/forms/createGroupForm.php" style="display: inline-block; padding: 10px 20px; 
+background-color: #8ac4f6; color: white; text-decoration: none; border-radius: 5px;
+font-weight: bold; position: absolute; top: 100px; right: 20px;">Create Group</a> 
